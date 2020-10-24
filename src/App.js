bd13c43bf8c22be09c47cbe318509272b5f13b9a
Rachel Wong <hello@rachelwonghw.com>
2020-10-24 13:36:18 -0400

    added routes

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React from 'react';
 import './App.css';
 import Header  from './components/Header';
 import Home from './pages/Home';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <div className="App">
         <Header />
-        <Home />
+        <Route exact path='/'>
+          <Home />
+        </Route>
       </div>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 

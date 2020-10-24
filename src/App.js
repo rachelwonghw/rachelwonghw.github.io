e755f04b0fab6ab2711d37bcedebff5b0780ba7e
Rachel Wong <hello@rachelwonghw.com>
2020-10-24 03:56:36 -0400

    remove unused code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import  Header  from './components/Header';
+import Header  from './components/Header';
 import Home from './pages/Home';
 
 function App() {
@@ -8,20 +8,6 @@ function App() {
     <div className="App">
       <Header />
       <Home />
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
     </div>
   );
 }

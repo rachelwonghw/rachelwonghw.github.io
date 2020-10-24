80c2fbd77317cfa57062188edd2cdad0d0f4ce67
Rachel Wong <hello@rachelwonghw.com>
2020-10-24 02:05:10 -0400

    initial draft of header

---
+++ src/components/Header.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  NavItem,
+  NavLink,
+ } from 'reactstrap';
+
+
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const onToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <Navbar dark expand='md' className='bg-transparent' fixed='top'>
+      <NavbarBrand href="/">Rachel Wong</NavbarBrand>
+      <NavbarToggler onClick={onToggle} />
+      <Collapse isOpen={isOpen} navbar>
+        <Nav navbar className='ml-auto'>
+          <NavItem>
+            <NavLink href='/'>Home</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href='/about'>About</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href='/employment'>Employment</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href='/contact'>Contact</NavLink>
+          </NavItem>
+        </Nav>
+      </Collapse>
+    </Navbar>
+  );
+}
+
+export default Header;
